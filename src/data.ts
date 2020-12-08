f0f55d57baa172dddc71d4b3f75333d4806ac271
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-12-08 18:30:45 +0100

    added new ojects to the staffs array

--- src/data.ts
+++ src/data.ts
@@ -3,88 +3,88 @@ export const staffs = [
       id: "1", 
       name: "Arc. Adeyemo Adekunle",
       rank: "Ag. HOD / Deputy Director",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Architecture",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl"
      }, 
      { 
       id: "2", 
       name: "Engr. O.C. Olowa",
       rank: "Deputy Director",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Mechanical Engineering",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     },
     { 
       id: "3", 
       name: "Engr. M.B. Jenyo",
       rank: "Deputy Director",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Electrical Engineering",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     }, 
     { 
       id: "4", 
       name: "Engr. Alege",
       rank: "Asst. Chief",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Mechanical Engineering",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     },
     { 
       id: "5", 
       name: "Arc. Abdulakeem",
       rank: "Principal Architect",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Architecture",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     }, 
     { 
       id: "6", 
       name: "Mr. Abdulakeem Ojuolape",
       rank: "Chief Admin Officer",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Administration",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     },
     { 
       id: "7", 
       name: "Engr. Abdulrasak Oluwanisola",
       rank: "Deputy Director",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Civil Engineering",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     },
     { 
       id: "8", 
       name: "Engr. Adua .T.",
       rank: "Deputy Director",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Maintenance",
+      gender: "Female",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     },
     { 
       id: "9", 
       name: "Engr. Bamidele Segun",
       rank: "Senior Engineer",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Electrical Engineering",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     }, 
     { 
       id: "10", 
       name: "Engr. Adeyi",
       rank: "Principal Engineer",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Biomedical Engineering",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     },
     { 
       id: "11", 
       name: "Engr. uthman Eletu",
       rank: "Senior Engineer",
-      unit: "Quantity Survey",
-      gender: "",
+      unit: "Electrical Engineering",
+      gender: "Male",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
     },
     { 
@@ -92,7 +92,15 @@ export const staffs = [
       name: "Mrs. Ruqayat Solagberu",
       rank: "Deputy Director",
       unit: "Quantity Survey",
-      gender: "",
+      gender: "Female",
       bio: "hdjdjdsk jaklasl sksklsl kklsskl" 
+    },
+    { 
+      id: "13", 
+      name: "Lawal, Abdulrafiu Kehinde",
+      rank: "Engineer I",
+      unit: "Electrical Engineering",
+      gender: "Male",
+      bio: "I am an Electrical Engineer and Web Developer" 
     },
       ];
